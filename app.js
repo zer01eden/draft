546ba747333f330e1ce79ada19983882f67c6a97
zero_jeden <zeroitjeden@gmail.com>
2024-07-28 22:10:29 +0200

    feat: new app.js (repeat: alert, confirm, prompt)

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+console.log('hi, this is draft..');
+var aleretResult = alert('start');
+console.log('alert result: ', aleretResult);
+var confirmResult = confirm('Are You ready?');
+console.log('confirm status: ', confirmResult);
+var promptResult = prompt('Add Your nick', 'nick');
+console.log(`welcome ${promptResult}`);
