3c15decd463c57b5c29572b40ee418315eb86d00
zero_jeden <zeroitjeden@gmail.com>
2024-08-02 20:27:04 +0200

    repeat loops for

---
+++ app_2.js
@@ -0,0 +1,17 @@
+//repeat loops - for
+
+'use script';
+
+var endIncrement = 6;
+
+for (var i = 0; i < endIncrement; i++) {
+	console.log(i);
+}
+
+console.log('------------');
+
+var endDecrement = 0;
+
+for (var j = 6; j > endDecrement; j--) {
+	console.log(j);
+}
