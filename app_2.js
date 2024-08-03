9da0537f2a7687cd5d3c70857ae9d8d6eab62c07
zero_jeden <zeroitjeden@gmail.com>
2024-08-03 16:30:16 +0200

    fix: repat loops for arrays

--- app_2.js
+++ app_2.js
@@ -15,3 +15,17 @@ var endDecrement = 0;
 for (var j = 6; j > endDecrement; j--) {
 	console.log(j);
 }
+
+// repeat loop for (arrays)
+var users = ['zero', 'one', 'two', 'tree'];
+var nums = [0, 1, 2, 3];
+
+for (var k = 0; k < users.length; k++) {
+	console.log(users[k]);
+}
+
+var sum = 0;
+for (var l = 0; l < nums.length; l++) {
+	sum = sum + nums[l];
+}
+console.log(`All sumnum is: ${sum}`);
