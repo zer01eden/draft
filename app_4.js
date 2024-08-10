0150879295ac2265db39f991fd8e964f63554243
zero_jeden <zeroitjeden@gmail.com>
2024-08-10 15:46:25 +0200

    fix: test hoisting

--- app_4.js
+++ app_4.js
@@ -15,3 +15,38 @@ console.log(add10(69)); //79
 console.log(add10(656)); //666
 console.log(add10(-17)); //-7
 console.log(add10(-10)); //0
+
+console.log('--------------');
+//hoisting
+
+// 1:
+
+//variables are dclared first
+//before code execution
+//so it will have undefined value
+
+console.log(a);
+
+var a = 1;
+
+// 2:
+// function declarations are hoisted whole
+// so it works before declarations in code
+
+console.log(add(1, 4));
+
+function add(a, b) {
+	return a + b;
+}
+
+// 3:
+// functions expressions beheve like
+// normal variables declarations
+// so it will have undifaned value
+// and trow error because you cannot call undefined
+
+console.log(addExpresion(1, 6));
+
+var addExpresion = function (a, b) {
+	return a + b;
+};
